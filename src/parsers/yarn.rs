1833d147ebbe4e170ab016586acb25e03999f465
takakuda <kei.takakuda.0415@icloud.com>
2023-12-01 19:01:46 +0900

    feat: Add yarn parser

---
+++ src/parsers/yarn.rs
@@ -0,0 +1,35 @@
+use std::path::{Path, PathBuf};
+use std::{error::Error, fs};
+use yarn_lock_parser::{parse_str, Entry};
+
+use crate::entities::package::{Package, Source};
+use crate::parsers::error::ParseError;
+use crate::parsers::Parser;
+
+pub struct YarnParser {}
+
+impl YarnParser {
+    pub fn new() -> Self {
+        YarnParser {}
+    }
+}
+
+impl Parser for YarnParser {
+    fn parse(&self, path: &PathBuf) -> Result<Vec<Package>, ParseError> {
+        let file = fs::read_to_string(path).unwrap();
+        let entries = parse_str(&file)?;
+
+        let packages = entries
+            .iter()
+            .map(|entry| Package {
+                name: entry.name.to_string(),
+                current_version: entry.version.to_string(),
+                latest_version: None,
+                source: Source::npm,
+                homepage: None,
+            })
+            .collect::<Vec<Package>>();
+
+        Ok(packages)
+    }
+}
